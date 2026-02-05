c8e695b20fbb859d9ed3682b3cf0aa566b84794f
rektdie <rektdie@gmail.com>
2026-02-05 01:54:55 +0100

    added SQLite db

---
+++ Camel Registry/CamelDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class CamelDb : DbContext
+{
+    public CamelDb(DbContextOptions<CamelDb> options)
+        : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Camels.db");
+    }
+
+    public DbSet<Camel> Camels => Set<Camel>();
+}
--- Camel Registry/Program.cs
+++ Camel Registry/Program.cs
@@ -1,5 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register DB
+builder.Services.AddDbContext<CamelDb>();
+
 var app = builder.Build();
+
+// Create DB
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CamelDb>();
+    db.Database.Migrate();
+}
 
 app.MapGet("/", () => "Hello World!");
 
