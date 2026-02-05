870398bcd24baad2454113d012f792db6a68d90a
rektdie <rektdie@gmail.com>
2026-02-05 02:04:54 +0100

    implemented Swagger

--- Camel Registry/Program.cs
+++ Camel Registry/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Register DB
 builder.Services.AddDbContext<CamelDb>();
 
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Create DB
@@ -12,6 +16,12 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<CamelDb>();
     db.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapGet("/", () => "Hello World!");
