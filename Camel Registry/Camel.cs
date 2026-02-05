fc52670fdc8c6d2c4b555e00acef0a80b9798f2e
rektdie <rektdie@gmail.com>
2026-02-05 01:23:42 +0100

    Camel entity

---
+++ Camel Registry/Camel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Camel
+{
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public uint Color { get; set; }
+	[Range(1, 2)]
+	public int HumpCount { get; set; }
+	public DateTime LastFed {  get; set; }
+}
